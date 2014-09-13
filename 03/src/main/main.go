29d879b9d5961a7fcb13382e8e4bf3bfddd54c0c
shellhead <mike.hunsinger@gmail.com>
2014-09-13 12:53:41 -0600

    Changes made to output for parsing, setup assignment 3

---
+++ 03/src/main/main.go
@@ -0,0 +1,24 @@
+// Author: Michael Hunsinger
+// Date:   Aug 24 2014
+// File:   main.go
+// Reads the specified file and prints out a list of tokens
+// Read the README.pdf for more information on compiling and running the file
+
+package main
+
+import (
+	"bytes"
+	"compiler"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	// create a new reader and initialze a parser with it
+	buf, _ := ioutil.ReadFile(os.Args[1])
+	reader := bytes.NewReader(buf)
+
+	p := new(compiler.Parser)
+	p.Scanner = compiler.Scanner{Reader: *reader}
+	p.SystemGoal()
+}
